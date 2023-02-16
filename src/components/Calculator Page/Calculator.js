92925565bda31d46faa9236a26c67e269505e317
alexanderblk <115581300+alexanderblk@users.noreply.github.com>
2023-02-16 20:18:09 +0000

    addded Navigation component

--- src/components/Calculator Page/Calculator.js
+++ src/components/Calculator Page/Calculator.js
@@ -3,14 +3,14 @@ import React from "react";
 const Calculator = () => {
     return (
         <div className="container">
-            <div className="row">
+            {/* <div className="row">
                 <div className="col-sm-9">
                     <h1>Budget Calculator</h1>
                     <div className="row">
                         <div className="col-8 col-sm-6">
                             <div className="row">
-                                <div className="col-8 col-sm-6">Col 1</div>
-                                <div className="col-4 col-sm-6">Col 2</div>
+                                <div className="col-8 col-sm-6"><PaymentInput /></div>
+                                <div className="col-4 col-sm-6"><Balance /></div>
                             </div>
                         </div>
                         <div className="col-4 col-sm-6">
@@ -18,7 +18,7 @@ const Calculator = () => {
                         </div>
                     </div>
                 </div>
-            </div>
+            </div> */}
 
         </div>
 
