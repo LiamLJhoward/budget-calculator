af87fb6786b3c20d401babde98efb9c6ded0c6da
LiamLJhoward <115423162+LiamLJhoward@users.noreply.github.com>
2023-02-16 18:44:05 +0000

    restruct

---
+++ src/components/Calculator Page/Balance.js
@@ -0,0 +1,3 @@
+import React from "react";
+import Calculator from './components/Calculator Page/Calculator';
+
--- src/components/Calculator Page/Calculator.js
+++ src/components/Calculator Page/Calculator.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Balance from './components/Calculator Page/Balance';
 
 const Calculator = () => {
     return (
@@ -10,7 +11,7 @@ const Calculator = () => {
                         <div className="col-8 col-sm-6">
                             <div className="row">
                                 <div className="col-8 col-sm-6">Col 1</div>
-                                <div className="col-4 col-sm-6">Col 2</div>
+                                <div className="col-4 col-sm-6">Balance</div>
                             </div>
                         </div>
                         <div className="col-4 col-sm-6">
