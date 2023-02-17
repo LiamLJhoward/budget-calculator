309418d39a80e73d4637a74f1020426508c31f5c
alexanderblk <115581300+alexanderblk@users.noreply.github.com>
2023-02-17 17:02:43 +0000

    added Hero Section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
+import Hero from './components/Hero/Hero';
 import Calculator from './components/Calculator/Calculator';
 import Balance from './components/Calculator/Balance/Balance';
 
@@ -7,7 +8,8 @@ function App() {
   return (
     <div>
       <Navigation />
-      <Calculator />
+      {/* <Calculator /> */}
+      <Hero />
     </div>
   );
 }
---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,23 @@
+import React from "react";
+import HeroImg from './heroimg.webp';
+import './Hero.css';
+
+
+const Hero = () => {
+    return (
+        <div className="container-fluid hero">
+            <div className="row">
+                <div className="col-md-6 col-sm-12 hero-img">
+                    <img src={HeroImg} className="img-fluid d-none d-md-block" alt='hero banner' />
+                </div>
+                <div className="col-md-6 col-sm-12 hero-content">
+                    <h1 className="hero-title">Take control of your fiances</h1>
+                    <p className="hero-subtitle">The easiest way to stay on top of your finances and reach your financial goals</p>
+                    <button className="hero-button">Get Started</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Hero;
