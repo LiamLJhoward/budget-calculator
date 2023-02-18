4dcb10f0618160d7a847de2769dc2c653ea73488
LiamLJhoward <115423162+LiamLJhoward@users.noreply.github.com>
2023-02-18 13:31:14 +0000

    changed reviews to testimonials

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Hero from './components/Hero/Hero';
 import CTA from './components/CTA Section/CTA';
 import Calculator from './components/Calculator/Calculator';
 import Balance from './components/Calculator/Balance/Balance';
-import Reviews from './components/Reviews/Reviews';
+import Testimonials from './components/Reviews/Testimonials';
 
 
 function App() {
@@ -14,7 +14,7 @@ function App() {
       {/* <Calculator /> */}
       <Hero />
       <CTA />
-      <Reviews />
+      <Testimonials />
     </div>
   );
 }
