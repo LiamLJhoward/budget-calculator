517be5333a3e309255812582a8dd61af6157da65
alexanderblk <115581300+alexanderblk@users.noreply.github.com>
2023-02-20 18:43:18 +0000

    completed CTA section

--- src/components/CTA Section/CTA.js
+++ src/components/CTA Section/CTA.js
@@ -1,39 +1,45 @@
 import React from "react";
-import './CTA.css';
-import Calculator from './calculator.webp';
-import FeatResp from './feat-responsive.svg';
-import FeatEasy from './feat-easy.svg';
-import FeatChart from './feat-chart.svg';
-
+import "./CTA.css";
+import Calculator from "./calculator.webp";
+import FeatResp from "./feat-responsive.svg";
+import FeatEasy from "./feat-easy.svg";
+import FeatChart from "./feat-chart.svg";
 
 const CTA = () => {
     return (
         <div className="container-fluid cta">
             <div className="row">
-                <div className="col-md-6 col-sm-12 order-md-1 order-2 cta-content">
-                    <h1 className="hero-title">Plan for a better future with our budget calculator</h1>
-                    <p className="hero-subtitle">This is the perfect tool for creating a solid financial plan. You can easily track your income, expenses, and savings.</p>
-                    <div className="container">
-                        <div className="features">
-                            <div className="feat1">
-                                <img src={FeatResp} alt='responsive icon' className="feat-icon" />
-                            </div>
-                            <div className="feat2">
-                                <img src={FeatEasy} alt='easy-to-use icon' className="feat-icon" />
-                            </div>
-                            <div className="feat3">
-                                <img src={FeatChart} alt='chart icon' className="feat-icon" />
-                            </div>
+                <div className="col-sm-12 col-lg-6 order-md-1 order-2 cta-content-container">
+                    <h1 className="cta-title">
+                        Plan for a better future with our budget calculator
+                    </h1>
+
+                    <div className="block-features">
+                        <div className="feature">
+                            <img src={FeatResp} alt="responsive icon" className="feature-icon" />
+                            <p className="feature-text">RESPONSIVE</p>
+                        </div>
+                        <div className="feature">
+                            <img src={FeatEasy} alt="easy-to-use icon" className="feature-icon" />
+                            <p className="feature-text">EASY TO USE</p>
+                        </div>
+                        <div className="feature">
+                            <img src={FeatChart} alt="chart icon" className="feature-icon" />
+                            <p className="feature-text">CUSTOM CHART</p>
                         </div>
                     </div>
-                    <button className="hero-button">Get Started</button>
+                    <p className="cta-subtitle">
+                        This is the perfect tool for creating a solid financial plan. You can easily track your income, expenses, and savings.
+                    </p>
+                    <button className="cta-button">Get Started</button>
                 </div>
-                <div className="col-md-6 col-sm-12 order-md-2 order-1 cta-image">
-                    <img src={Calculator} className='img-fluid' alt='calculator' />
+                <div className="col-sm-12 col-lg-6 order-md-2 order-1 cta-image-container">
+                    <img src={Calculator} className="img-fluid cta-image" alt="calculator" />
                 </div>
             </div>
         </div>
-    )
-}
+
+    );
+};
 
 export default CTA;
