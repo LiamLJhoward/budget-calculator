0b2f7033022f4ba1118ee13c41470c93ad904928
alexanderblk <115581300+alexanderblk@users.noreply.github.com>
2023-02-22 18:58:39 +0000

    deleted useless files

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Hero from './components/Hero/Hero';
 import CTA from './components/CTA Section/CTA';
-import Calculator from './components/Calculator/Calculator';
-import Balance from './components/Calculator/Balance/Balance';
 import Testimonials from './components/Testimonials/Testimonials';
 import Tips from './components/Tips & Tricks/Tips';
 import Footer from './components/Footer/Footer';
@@ -14,12 +12,12 @@ function App() {
   return (
     <div>
       <Navigation />
-      {/* <Calculator /> */}
-      <Hero />
+      <Inpute />
+      {/* <Hero />
       <CTA />
       <Testimonials />
       <Tips />
-      <Footer />
+      <Footer /> */}
     </div>
   );
 }
--- src/components/Calculator/Payments/Payments.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Payments = () => {
-    return (
-        <div></div>
-    )
-}
-
-export default Payments;
